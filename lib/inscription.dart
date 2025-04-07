71c4666f5fe4507c9143f14074afc2cd5dd4ec9d
elievik <vikoumkoudzoelie@gmail.com>
2025-04-07 04:13:03 +0000

    Modofication a la page d'inscription

--- lib/inscription.dart
+++ lib/inscription.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
 
-class InscriptionScreen extends StatelessWidget {
+class InscriptionScreen extends StatefulWidget {
   @override
+  _InscriptionScreenState createState() => _InscriptionScreenState();
+}
+
+class _InscriptionScreenState extends State<InscriptionScreen> {
+  final TextEditingController _nomControler = TextEditingController();
+  final TextEditingController _prenomController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+  final TextEditingController _telephoneController = TextEditingController();
+  final TextEditingController _dateNaissanceController =
+      TextEditingController();
+  final TextEditingController _villeController = TextEditingController();
+  final TextEditingController _paysController = TextEditingController();
+  final TextEditingController _adresseController = TextEditingController();
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Inscription')),
-      body: Center(child: Text('Écran d\'Inscription')),
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        title: RichText(
+          text: TextSpan(
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Sans',
+            ),
+            children: [
+              TextSpan(text: 'e', style: TextStyle(color: Colors.blue)),
+              TextSpan(text: 'tontine', style: TextStyle(color: Colors.black)),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
