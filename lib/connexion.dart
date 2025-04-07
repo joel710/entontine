1182a91420b8f1b099c80f274a78d28c7a926f94
elievik <vikoumkoudzoelie@gmail.com>
2025-04-07 03:21:51 +0000

    La page de connexion a été modifiée

--- lib/connexion.dart
+++ lib/connexion.dart
@@ -1,11 +1,107 @@
 import 'package:flutter/material.dart';
 
-class ConnexionScreen extends StatelessWidget {
+import 'inscription.dart';
+
+class ConnexionScreen extends StatefulWidget {
+  @override
+  _ConnexionScreenState createState() => _ConnexionScreenState();
+}
+
+class _ConnexionScreenState extends State<ConnexionScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Connexion')),
-      body: Center(child: Text('Écran de Connexion')),
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        title: RichText(
+          text: TextSpan(
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Sans',
+            ),
+            children: [
+              TextSpan(text: 'e', style: TextStyle(color: Colors.blue)),
+              TextSpan(text: 'tontine', style: TextStyle(color: Colors.black)),
+            ],
+          ),
+        ),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Connection',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: const Color.fromARGB(255, 5, 112, 253),
+                fontFamily: 'Sans',
+              ),
+            ),
+            SizedBox(height: 30),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Mot de passe',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'Se connecter',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 5, 112, 253),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => InscriptionScreen()),
+                );
+              },
+              child: Text(
+                's\'inscrire',
+                style: TextStyle(
+                  color: const Color.fromARGB(255, 5, 112, 253),
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
